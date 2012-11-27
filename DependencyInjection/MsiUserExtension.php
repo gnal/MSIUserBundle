abaef244cb5db62bb875ca2ba10c0997138b3e28
gnal <qwertypianox@gmail.com>
2012-11-27 11:12:43 -0500

    fix

--- DependencyInjection/MsiUserExtension.php
+++ DependencyInjection/MsiUserExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Msi\Bundle\CmfBundle\DependencyInjection;
+namespace Msi\Bundle\UserBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
