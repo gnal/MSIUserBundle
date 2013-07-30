fd294e9b94efbbdb334248acd784109b5ece3ff1
gnal <qwertypianox@gmail.com>
2013-07-30 14:12:12 -0400

    fix

--- Admin/UserAdmin.php
+++ Admin/UserAdmin.php
@@ -2,8 +2,8 @@
 
 namespace Msi\UserBundle\Admin;
 
-use Msi\CmfBundle\Admin\Admin;
-use Msi\CmfBundle\Grid\GridBuilder;
+use Msi\AdminBundle\Admin\Admin;
+use Msi\AdminBundle\Grid\GridBuilder;
 use Symfony\Component\Form\FormBuilder;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
@@ -79,7 +79,7 @@ class UserAdmin extends Admin
             $roles['ROLE_SUPER_ADMIN'] = 'super admin';
             $roles['ROLE_ADMIN'] = 'admin';
 
-            foreach ($this->container->getParameter('msi_cmf.admin_ids') as $id) {
+            foreach ($this->container->getParameter('msi_admin.admin_ids') as $id) {
                 $label = $this->container->get($id)->getLabel(1, 'en');
                 $roles['ROLE_'.strtoupper($id).'_CREATE'] = $label.' | create';
                 $roles['ROLE_'.strtoupper($id).'_READ'] = $label.' | read';
@@ -111,6 +111,6 @@ class UserAdmin extends Admin
 
     public function postLoad(ArrayCollection $collection)
     {
-        $this->container->get('msi_cmf.bouncer')->operatorFilter($collection);
+        $this->container->get('msi_admin.bouncer')->operatorFilter($collection);
     }
 }
